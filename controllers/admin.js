5b9ed2a24000cd02ee61fcde63d2e63221183d60
BiggaHD <dzenis.hankusic@gmail.com>
2019-07-04 23:43:53 +0200

    adding auth

--- controllers/admin.js
+++ controllers/admin.js
@@ -23,8 +23,6 @@ exports.postAddProduct = (req, res, next) => {
   product
     .save()
     .then(result => {
-      // console.log(result);
-      console.log("Created Product");
       res.redirect("/admin/products");
     })
     .catch(err => {
@@ -77,6 +75,8 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.find()
+    // .select('title price -_id')
+    // .populate('userId', 'name')
     .then(products => {
       console.log(products);
       res.render("admin/products", {
